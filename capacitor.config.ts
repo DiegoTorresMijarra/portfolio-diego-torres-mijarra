c0ca5df9adc7c4f2f705f5460c66f47344c6247c
Diego Torres Mijarra <diegoto999@gmail.com>
2024-04-17 18:26:02 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PortfolioDiegoTorresMijarra',
+  webDir: 'www'
+};
+
+export default config;
