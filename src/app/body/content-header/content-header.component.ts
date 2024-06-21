bbb53ca0dd57c7f30ebbf02dc71053f4eb642fd9
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-06-21 17:52:48 +0200

    fix content header

--- src/app/body/content-header/content-header.component.ts
+++ src/app/body/content-header/content-header.component.ts
@@ -12,30 +12,31 @@ export class ContentHeaderComponent implements OnInit {
   ngOnInit() {
   }
 
-  constructor(private platform: Platform) {
-  }
-
-  downloadCV() {
-    const fileUrl = 'assets/CV_DiegoTorres2024(tecnico).pdf';
-
-    if (this.platform.is('capacitor')) {
-      this.downloadMobile(fileUrl);
-    } else {
-      this.downloadWeb(fileUrl);
-    }
-  }
-
-  downloadWeb(fileUrl: string) {
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'CV_DiegoTorres2024.pdf';
-    link.target = '_blank';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-
-  private downloadMobile(fileUrl: string) {
-
-  }
+  //
+  // constructor(private platform: Platform) {
+  // }
+  //
+  // downloadCV() {
+  //   const fileUrl = 'assets/CV_DiegoTorres2024(tecnico).pdf';
+  //
+  //   if (this.platform.is('capacitor')) {
+  //     this.downloadMobile(fileUrl);
+  //   } else {
+  //     this.downloadWeb(fileUrl);
+  //   }
+  // }
+  //
+  // downloadWeb(fileUrl: string) {
+  //   const link = document.createElement('a');
+  //   link.href = fileUrl;
+  //   link.download = 'CV_DiegoTorres2024.pdf';
+  //   link.target = '_blank';
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // }
+  //
+  // private downloadMobile(fileUrl: string) {
+  //
+  // }
 }
